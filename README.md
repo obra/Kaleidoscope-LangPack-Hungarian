9a9bfc805c4eb08458604931bc092f8431aec913
Gergely Nagy <algernon@madhouse-project.org>
2017-01-21 10:07:36 +0100

    Initial import
    Lifted out from algernon/Model01-Sketch.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

---
+++ README.md
@@ -0,0 +1,57 @@
+# Akela-LangPack-Hungarian
+
+![status][st:experimental]
+
+ [st:stable]: https://img.shields.io/badge/stable-✔-black.png?style=flat&colorA=44cc11&colorB=494e52
+ [st:broken]: https://img.shields.io/badge/broken-X-black.png?style=flat&colorA=e05d44&colorB=494e52
+ [st:experimental]: https://img.shields.io/badge/experimental----black.png?style=flat&colorA=dfb317&colorB=494e52
+
+Hungarian language pack for the AKELA plugin library. This plugin introduces a
+number of new key codes, which can be used to input Hungarian symbols on any
+operating system that supports a `Compose` key. The package has been tested
+under GNOME and Windows with WinCompose.
+
+## Using the plugin
+
+To use the plugin, one needs to include the header, and add the new keycodes to
+the keymap.
+
+## Plugin symbols
+
+The plugin provides the `Hungarian` object, and the following new keycodes:
+
+### HUN_AA
+
+> Short for "a acute". The keycode results in `á`, or `Á` when shifted.
+
+### HUN_OA
+
+> Short for "o acute". The keycode results in `ó`, or `Ó` when shifted.
+
+### HUN_OU
+
+> Short for "o umlaut". The keycode results in `ö`, or `Ö` when shifted.
+
+### HUN_ODA
+
+> Short for "o double-acute". The keycode results in `ő`, or `Ő` when shifted.
+
+### HUN_EA
+
+> Short for "e acute". The keycode results in `é`, or `É` when shifted.
+
+### HUN_UA
+
+> Short for "u acute". The keycode results in `ú`, or `Ú` when shifted.
+
+### HUN_UU,
+
+> Short for "u umlaut". The keycode results in `ü`, or `Ü` when shifted.
+
+### HUN_UDA,
+
+> Short for "u double-acute". The keycode results in `ű`, or `Ű` when shifted.
+
+### HUN_IA
+
+> Short for "i acute". The keycode results in `í`, or `Í` when shifted.
---
+++ src/Akela/LangPack-Hungarian.cpp
@@ -0,0 +1,138 @@
+/* -*- mode: c++ -*-
+ * Akela -- Animated Keyboardio Extension Library for Anything
+ * Copyright (C) 2016, 2017  Gergely Nagy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <Akela-LangPack-Hungarian.h>
+
+typedef enum {
+  AA,
+  OA,
+  OU,
+  ODA,
+  EA,
+  UA,
+  UU,
+  UDA,
+  IA,
+} HungarianSymbol;
+
+namespace Akela {
+  namespace LangPack {
+
+    Key
+    Hungarian_::eventHandlerHook (Key mappedKey, byte row, byte col, uint8_t keyState) {
+      if (mappedKey.raw < HUNGARIAN_FIRST || mappedKey.raw > HUNGARIAN_LAST)
+        return mappedKey;
+
+      if (!key_toggled_on (keyState))
+        return Key_NoKey;
+
+      bool needShift = Keyboard.isModifierActive (Key_LShift.keyCode);
+
+      Keyboard.press (Key_RAlt.keyCode);
+      Keyboard.sendReport ();
+      delay (10);
+      Keyboard.release (Key_RAlt.keyCode);
+      Keyboard.sendReport ();
+      delay (10);
+
+      HungarianSymbol symbol = (HungarianSymbol) (mappedKey.raw - HUNGARIAN_FIRST);
+      Key accent;
+      uint8_t kc = 0;
+
+      accent.flags = KEY_FLAGS;
+
+      switch (symbol) {
+      case AA:
+        kc = Key_A.keyCode;
+        accent.raw = Key_Quote.raw;
+        break;
+      case OA:
+        kc = Key_O.keyCode;
+        accent.raw = Key_Quote.raw;
+        break;
+      case OU:
+        kc = Key_O.keyCode;
+        accent.raw = Key_Quote.raw;
+        accent.flags |= SHIFT_HELD;
+        break;
+      case ODA:
+        kc = Key_O.keyCode;
+        accent.raw = Key_Equals.raw;
+        break;
+      case EA:
+        kc = Key_E.keyCode;
+        accent.raw = Key_Quote.raw;
+        break;
+      case UA:
+        kc = Key_U.keyCode;
+        accent.raw = Key_Quote.raw;
+        break;
+      case UU:
+        kc = Key_U.keyCode;
+        accent.raw = Key_Quote.raw;
+        accent.flags |= SHIFT_HELD;
+        break;
+      case UDA:
+        kc = Key_U.keyCode;
+        accent.raw = Key_Equals.raw;
+        break;
+      case IA:
+        kc = Key_I.keyCode;
+        accent.raw = Key_Quote.raw;
+        break;
+      }
+
+      if (accent.flags & SHIFT_HELD)
+        Keyboard.press (Key_LShift.keyCode);
+      else
+        Keyboard.release (Key_LShift.keyCode);
+      Keyboard.sendReport ();
+      delay(10);
+
+      Keyboard.press (accent.keyCode);
+      Keyboard.sendReport ();
+      Keyboard.release (accent.keyCode);
+      Keyboard.sendReport ();
+
+      if (needShift)
+        Keyboard.press (Key_LShift.keyCode);
+      else
+        Keyboard.release (Key_LShift.keyCode);
+
+      delay (10);
+      Keyboard.press (kc);
+      Keyboard.sendReport ();
+      delay(10);
+      Keyboard.release (kc);
+      Keyboard.sendReport ();
+      delay (10);
+
+      return Key_NoKey;
+    }
+
+    Hungarian_::Hungarian_ (void) {
+    }
+
+    void
+    Hungarian_::begin (void) {
+      event_handler_hook_add (eventHandlerHook);
+    }
+  }
+}
+
+Akela::LangPack::Hungarian_ Hungarian;
